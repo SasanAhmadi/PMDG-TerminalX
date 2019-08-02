92b4f731ebfe0c5fc1d7d041ef652bbe33d80d82
Sasan Ahmadi <sasan_developer@msn.com>
2019-08-02 18:26:04 -0400

    Added to method replacing ToString method with specific DmsToString and DdToString functions

--- PMDG-TerminalX/Base/GeoPointBase.cs
+++ PMDG-TerminalX/Base/GeoPointBase.cs
@@ -17,10 +17,9 @@ namespace PMDG_TerminalX.Base
             set
             {
                 if (value.Degrees < 0)
-                    _decimalValue = -(value.Seconds / 3600) - (value.Minutes / 60) + value.Degrees;
+                    DecimalValue = -(value.Seconds / 3600) - (value.Minutes / 60) + value.Degrees;
                 else
-                    _decimalValue = (value.Seconds / 3600) + (value.Minutes / 60) + value.Degrees;
-                _dmsValue = value;
+                    DecimalValue = (value.Seconds / 3600) + (value.Minutes / 60) + value.Degrees;
 
                 IdentifyHemisphereEvent(this, _decimalValue);
             }
@@ -33,6 +32,10 @@ namespace PMDG_TerminalX.Base
                 _dmsValue.Degrees = (int)decimal.Truncate(value);
                 _dmsValue.Minutes = (int)decimal.Truncate((value - decimal.Truncate(value)) * 60);
                 _dmsValue.Seconds = decimal.Round((((value - decimal.Truncate(value)) * 60) - DMS.Minutes) * 60, 1);
+
+                _ddValue.Degrees = (int)decimal.Truncate(value);
+                _ddValue.Seconds = decimal.Round((value - decimal.Truncate(value)) * 60, 6);
+
                 _decimalValue = value;
 
                 IdentifyHemisphereEvent(this, _decimalValue);
@@ -42,12 +45,19 @@ namespace PMDG_TerminalX.Base
         public (int Degrees, decimal Seconds) DD
         {
             get { return _ddValue; }
-            set { _ddValue = value; }
+            set {
+                DecimalValue = decimal.Round(value.Seconds / 60M, 6) + value.Degrees;
+            }
         }
 
-        public override string ToString()
+        public virtual string ToDmsString()
         {
-            return _dmsValue.Degrees.ToString("#;#;0") + "°" + _dmsValue.Minutes.ToString("#;#;0") + "'" + _dmsValue.Seconds.ToString("00.#;00.#;00.0") + "\"";
+            return _dmsValue.Degrees.ToString("0;0;0") + "°" + _dmsValue.Minutes.ToString("0;0;0") + "'" + _dmsValue.Seconds.ToString("00.0;00.0;00.0") + "\"";
+        }
+
+        public virtual string ToDdString()
+        {
+            return _ddValue.Degrees.ToString("0;0;0") + " " + _ddValue.Seconds.ToString("0.000000;0.000000;0.000000");
         }
     }
 }
