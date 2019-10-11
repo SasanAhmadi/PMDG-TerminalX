b1a92ba37435a6b018236fcb2b10d5b67d2d9050
Sasan Ahmadi <sasan_developer@msn.com>
2019-10-11 19:12:39 -0400

    Remove unused waypoint class

--- PMDG-TerminalX/NavAid/Airport.cs
+++ PMDG-TerminalX/NavAid/Airport.cs
@@ -9,7 +9,6 @@ namespace PMDG_TerminalX.NavAid
         public string Description { get; set; }
 
         public List<Fix> Fixes { get; set; }
-        public List<Waypoint> Waypoints { get; set; }
         public List<Runway> Runways { get; set; }
         public List<Sid> Sids { get; set; }
         public List<Star> Star { get; set; }
