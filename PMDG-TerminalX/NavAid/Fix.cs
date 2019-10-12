831ecdc02cc71de515e5caef8771b9de74599fb9
Sasan Ahmadi <sasan_developer@msn.com>
2019-10-11 20:02:31 -0400

    Added initial Fix class implemenation

--- PMDG-TerminalX/NavAid/Fix.cs
+++ PMDG-TerminalX/NavAid/Fix.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using PMDG_TerminalX.Geo;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PMDG_TerminalX.NavAid
 {
-    class Fix
+    public class Fix
     {
+        public Fix(string name, GeoLocation location)
+        {
+            Name = name;
+            Location = location;
+        }
+        public GeoLocation Location { get; set; }
+        public string Name { get; set; }
+        public string ParseFix()
+        {
+            return "FIX " + Name + " LATLON " + Location.ToDdString();
+        }
     }
 }
